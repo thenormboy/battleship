aa575ec437e7b3624cef6afe4438a9213e96a078
Bleaze Perris <bperriscode@gmail.com>
2023-11-22 13:42:12 -0500

    added board displays for both players

--- src/index.js
+++ src/index.js
@@ -15,9 +15,44 @@ function displayHeader() {
 }
 
 function displayPlayerOneBoard() {
+    const playerOneGameboard = document.querySelector('.player-one-gameboard');
 
+    (Game.getActiveGameboard().getBoard()).forEach((rowCell) => {
 
+        rowCell.forEach((columnCell) => {
+
+            const boardCell = document.createElement('div')
+            boardCell.textContent = columnCell
+            boardCell.setAttribute('id', columnCell)
+            boardCell.classList.add('player-one-cells')
+            boardCell.style.width = '10%'
+            boardCell.style.height = '10%'
+            playerOneGameboard.appendChild(boardCell)
+
+        })
+    })
+}
+
+function displayPlayerTwoBoard() {
+    const playerTwoGameboard = document.querySelector('.player-two-gameboard');
+
+    (Game.getActiveGameboard().getBoard()).forEach((rowCell) => {
+
+        rowCell.forEach((columnCell) => {
+
+            const boardCell = document.createElement('div')
+            boardCell.textContent = columnCell
+            boardCell.setAttribute('id', columnCell)
+            boardCell.classList.add('player-two-cells')
+            boardCell.style.width = '10%'
+            boardCell.style.height = '10%'
+            playerTwoGameboard.appendChild(boardCell)
+
+        })
+    })
 }
 
 displayHeader()
 displayPlayerOneBoard()
+Game.switchActiveBoard()
+displayPlayerTwoBoard()
